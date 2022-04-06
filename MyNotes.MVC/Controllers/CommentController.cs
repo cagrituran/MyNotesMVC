339c4129f61ff872d496605e5dea3fbb801b38dd
cagrituran <cagri.turann@hotmail.com>
2022-04-06 21:28:04 +0300

    Register and Activation process has been done

--- MyNotes.MVC/Controllers/CommentController.cs
+++ MyNotes.MVC/Controllers/CommentController.cs
@@ -53,7 +53,7 @@ namespace MyNotes.MVC.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public ActionResult Create(Comment comment, int? noteId)
         {
             ModelState.Remove("CreatedOn");
